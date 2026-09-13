543223d217d7105caba506274fd7235a3ac7d061
stevens limage <ste.limage@duocuc.cl>
2026-09-13 01:02:46 -0300

    desarrollo completo

--- src/main/java/dev/rampmaster/Administrativo.java
+++ src/main/java/dev/rampmaster/Administrativo.java
@@ -1,11 +1,19 @@
 package dev.rampmaster;
 
-import java.util.HashSet;
-
 public class Administrativo extends Usuario implements Colaborable {
+
+    public Administrativo(String run, String primerNombre, String segundoNombre, String primerApellido,
+                          String segundoApellido, Integer edad, String correoIns) {
+        super(run, primerNombre, segundoNombre, primerApellido, segundoApellido, edad, correoIns);
+    }
 
     @Override
     public int usarJunaColaborable(int valorAlmuerzo) {
-        return 0;
+        return valorAlmuerzo;
+    }
+
+    @Override
+    public String toString() {
+        return "[Administrativo] " + super.toString();
     }
 }
--- src/main/java/dev/rampmaster/Docente.java
+++ src/main/java/dev/rampmaster/Docente.java
@@ -4,33 +4,42 @@ import java.util.HashSet;
 
 public class Docente extends Usuario implements Colaborable {
 
-
     private HashSet<Asignatura> asignaturas;
 
-    public Administrativo(String nombre, String apellido, String correoIns, String run, HashSet<Asignatura> asignaturas) {
-        super(nombre, apellido, correoIns, run);
-        this.asignaturas = asignaturas;
+    public Docente(String run, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido,
+                   Integer edad, String correoIns,
+                   HashSet<Asignatura> asignaturas) {
+        super(run, primerNombre, segundoNombre, primerApellido,
+                segundoApellido, edad, correoIns);
+        this.asignaturas = (asignaturas != null) ? asignaturas : new HashSet<>();
     }
 
     public HashSet<Asignatura> getAsignaturas() {
-        return asignaturas;
-    }
+        return asignaturas;}
 
     public void setAsignaturas(HashSet<Asignatura> asignaturas) {
-        this.asignaturas = asignaturas;
+        this.asignaturas = (asignaturas != null) ? asignaturas : new HashSet<>();
     }
 
-    public void agregarAsignatura(String asignatura){
+    public void agregarAsignatura(Asignatura asignatura) {
+        if (asignatura == null)
+            throw new IllegalArgumentException("La asignatura no puede estar vacio");
         asignaturas.add(asignatura);
-
-
     }
-    public void eliminarAsignatura(String){
 
+    public void eliminarAsignatura(Asignatura asignatura) {
+        if (asignatura == null)
+            throw new IllegalArgumentException("La asignatura no puede estar vacio");
+        asignaturas.remove(asignatura);
     }
 
     @Override
     public int usarJunaColaborable(int valorAlmuerzo) {
-        return 0;
+        return valorAlmuerzo;
+    }
+
+    @Override
+    public String toString() {
+        return "[Docente] " + super.toString() + " | Asignaturas: " + asignaturas.size();
     }
 }
